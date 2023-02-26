156406d80a7230cdbef7c09e4bb7162ecf3ea5ad
Connor Jarrett <hello@connorjarrett.com>
2023-02-26 13:08:34 +0000

    Added main files

---
+++ chat.js
@@ -0,0 +1,96 @@
+var messageHistory = []
+
+function escapeHtml(unsafe) {
+    unsafe = unsafe.replaceAll("<br>","%LINEBREAK%")
+
+    let safe =  unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+    
+    safe = safe.replaceAll("%LINEBREAK%","<br>")
+    
+    return safe
+ }
+
+function send(message,from) {
+    message = escapeHtml(message)
+
+    if (from != "user" && from != "bot") {
+        console.error("Invalid Sender, only 'user' or 'bot' is accepted")
+        return false
+    }
+
+    messageHistory.push({"from":from,"message":message})
+
+    let bubble = document.createElement("span")
+    bubble.classList = "bubble"
+    bubble.dataset.sender = from
+    bubble.innerHTML = message
+
+    $(".messages-container")[0].appendChild(bubble)
+    $(".messages-container").scrollTop($(".messages-container")[0].scrollHeight)
+
+    let button = $(".submit")[0]
+    if (!hasBotGone()) {
+        button.classList.add("unavailable")
+    } else {
+        button.classList.remove("unavailable")
+    }
+}
+
+function askReply(question) {
+    // Add typing bubble
+    let bubble = document.createElement("span")
+    bubble.classList = "bubble typing"
+    bubble.dataset.sender = "bot"
+    $(".messages-container")[0].appendChild(bubble)
+    $(".messages-container").scrollTop($(".messages-container")[0].scrollHeight)
+
+    var reply = "this is a reply to that"
+    console.log(`Asking reply for '${question}'`)
+
+    // Process Response
+    reply = process(question)
+
+
+    // Remove  typing bubble
+    setTimeout(function(){
+        bubble.remove()
+        send(reply, "bot")
+    },15*Math.min(Math.max(question.length, 150), 2500))
+}
+
+function hasBotGone() {
+    var hasBotGone = true
+
+    $(".messages-container .bubble:not(.typing)").each(function(index){
+        if (index == $(".messages-container .bubble:not(.typing)").length - 1) {
+            let sender = ($(this)[0].dataset.sender)
+            
+            if (sender == "user") {
+                hasBotGone = false
+            }
+        }
+    })
+
+    return hasBotGone
+}
+
+$("form").submit(function(e){
+    e.preventDefault();
+
+    let data = $("form").serializeArray()
+    let message = data[0]["value"]
+
+
+    if (!message || !hasBotGone()) {
+        return false
+    }
+
+    $("#message-box")[0].value = ""
+    send(message,"user")
+    askReply(message)
+})
---
+++ process.js
@@ -0,0 +1,190 @@
+// Synonyms and actions
+const match = [
+    [["say","repeat","recite","parrot","mirror","reiterate"],"ECHO",false], 
+    [["hello","good morning","good afternoon","good evening","hi","hello world"],"GREET",true],
+]
+
+function getAction(word, question = null) {
+    // If the word is a match, get the action
+    for (let i=0; i<match.length; i++) {
+        let synonyms = match[i][0]
+        
+        if (synonyms.includes(word.toLowerCase())) {
+            if (question && (question.toLowerCase() != word.toLowerCase()) && match[i][2] == true ) {
+                return false;
+            } else {
+                var action = match[i][1]
+            }
+        }
+    }
+
+    return action
+}
+
+function breakup(question) {
+    var broken = []
+    let split = question.split(" ")
+
+    var currentAction = ""
+    var currentSentence = ""
+
+    for (x=0; x<split.length; x++) {
+        let word = split[x]
+        
+        if (getAction(word, question)) {
+            broken.push({"action":currentAction,"sentence":currentSentence})
+            currentAction = getAction(word, question)
+            currentSentence = ""
+        } else {
+            currentSentence = `${currentSentence} ${word}`
+
+            if (currentSentence[0] == " ") {
+                currentSentence = currentSentence.slice(1)
+            }
+        }
+
+    }
+
+    broken.push({"action":currentAction,"sentence":currentSentence})
+    
+    if (broken.length > 1) {
+        return broken.slice(1)
+    } else {
+        return broken
+    }
+}
+
+function execute(action, params) {
+    if (!action) {
+        return null
+    }
+
+    if (action == "ECHO") {
+        console.log(params)
+        if (params[0] == "'" || params[0] == '"') {
+            params = params.slice(1)
+        }
+
+        if (params.at(-1) == "'" || params.at(-1) == '"') {
+            params = params.slice(0,-1)
+        }
+
+        return params
+    } else if (action == "GREET") {
+        return "Hello!"
+    }
+}
+
+
+function process(question) {
+    const broken = breakup(question)
+    var out = ""
+
+    for (i=0; i<broken.length; i++) {
+        let breakAnswer = execute(broken[i]["action"], broken[i]["sentence"])
+
+        if (breakAnswer) {
+            out = out + breakAnswer
+        }
+    }
+
+    // Unsure, google it
+    if (!out) {
+        out = ""
+
+        // Knowledge Graph
+        $.ajax({
+            type: "GET",
+            url:"https://kgsearch.googleapis.com/v1/entities:search",
+            async: false,
+            data: {
+                key: "AIzaSyDA0P-tRZEytWhKqX_D_Z-Ce8hNARA3vAY",
+                indent: true,
+                limit: 1,
+                query: question
+            },
+        
+            success: function(data){
+                let items = data["itemListElement"]
+                
+                for (let y=0; y<items.length; y++) {
+                    let item = items[y]
+
+                    if (item["resultScore"] > 0.1) {
+                        let result = item["result"]
+
+                        out = `I've Googled "${question}", here's what I found
+                        <br><br>
+                        ${result["detailedDescription"]["articleBody"]} under
+                        the categories ${result["@type"].toString()}`
+                    }
+                }
+            }
+        })
+
+        if (!out) {
+            // Google Search
+            $.ajax({
+                type: "GET",
+                url:"https://www.googleapis.com/customsearch/v1",
+                async: false,
+                data: {
+                    key: "AIzaSyDA0P-tRZEytWhKqX_D_Z-Ce8hNARA3vAY",
+                    cx: "032d94750ca9d496a",
+                    q: question
+                },
+            
+                success: function(data){
+                    const blockedSties = [
+                        "www.instagram.com",
+                        "www.linkedin.com",
+                        "dodgingtherain.com",
+                        "www.facebook.com",
+                        "twitter.com"
+                    ]
+
+                    const prioritySites = [
+                        "connorjarrett.com",
+                        "cade.dev"
+                    ]
+                    
+                    let priority;
+                    let picked = []
+
+                    let items = data["items"]
+
+                    if (items) {
+                        for (let x=0; x<items.length; x++) {
+                            let item = items[x]
+
+                            if (blockedSties.includes(item["displayLink"])) {
+                                continue
+                            } else {
+                                if (prioritySites.includes(item["displayLink"])) {
+                                    priority = item
+                                } else {
+                                    picked.push(item)
+                                }
+                            }
+                        }
+
+                        if (!priority) {
+                            priority = picked[0]
+                        }
+
+                        console.log(priority)
+
+                        out = `Here's what Google said:<br><br>
+                        ${priority["snippet"]}<br>-----<br>
+                        ${priority["link"]}
+                        `
+                    } else {
+                        out = "I haven't found any results for that online"
+                    }
+                }
+            })
+        }
+    }
+
+    return out
+}
